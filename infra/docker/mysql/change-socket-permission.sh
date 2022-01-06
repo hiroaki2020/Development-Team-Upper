48e715016ee45992b03e62b49f3ecf2f389cb8c5
hiroaki <pghiroakisince2020@gmail.com>
2022-01-06 22:37:14 +0900

    bug fixed

--- infra/docker/mysql/change-socket-permission.sh
+++ infra/docker/mysql/change-socket-permission.sh
@@ -2,15 +2,7 @@
 
 set -e
 
-until mysqladmin -s ping
-do
-  sleep 1
-done
-while mysqladmin -s ping
-do
-  sleep 1
-done
-until mysqladmin -s ping && [ -S /run/mysql-socket/mysql.sock ]
+until [ -f /var/lib/mysql-files/mysql-init-complete ] && mysqladmin -s ping && [ -S /run/mysql-socket/mysql.sock ]
 do
   sleep 1
 done
--- infra/docker/mysql/docker-entrypoint-wrapper.sh
+++ infra/docker/mysql/docker-entrypoint-wrapper.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+if [ -f /var/lib/mysql-files/mysql-init-complete ]; then
+  rm /var/lib/mysql-files/mysql-init-complete
+fi
 crond
 tail -Fq /var/log/mysql/general.log /var/log/mysql/slow-query.log &
 /change-socket-permission.sh &
