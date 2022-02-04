a306fc86e37224a9aeb28672fc4571caa84f0330
hiroaki <pghiroakisince2020@gmail.com>
2022-02-04 16:11:01 +0900

    change IPC between containers from UNIX domain socket to TCP

--- infra/docker/mysql/change-socket-permission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-until [ -f /var/lib/mysql-files/mysql-init-complete ] && mysqladmin -s ping && [ -S /run/mysql-socket/mysql.sock ]
-do
-  sleep 1
-done
-exec chmod 660 /run/mysql-socket/mysql.sock
--- infra/docker/mysql/docker-entrypoint-wrapper.sh
+++ infra/docker/mysql/docker-entrypoint-wrapper.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-if [ -f /var/lib/mysql-files/mysql-init-complete ]; then
-  rm /var/lib/mysql-files/mysql-init-complete
-fi
 crond
 tail -Fq /var/log/mysql/general.log /var/log/mysql/slow-query.log &
-/change-socket-permission.sh &
 exec /entrypoint.sh "$@"
